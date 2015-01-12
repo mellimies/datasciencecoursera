680e86c603ccf957ac641e252193d7f109f39543
Jaska <jaska@Jaakko-MacBook-Pro.local>
2015-01-12 23:02:39 +0200

    Assignment 1, part 2 (complete)
    complete.R passed tests, first fail as I forgot to assign column names :(

---
+++ R Programming/Assignment 1/complete.R
@@ -0,0 +1,22 @@
+source('files.R')
+
+complete <- function(directory, id = 1:332) {
+  
+  csv_files <- get_csv_files(directory, id)
+  num_files <- length(csv_files)
+
+  v_id = numeric(num_files)
+  v_nobs = numeric(num_files)
+  
+  i_file <- 1
+  for(i in id) {
+    print(i)
+    t <- read.csv(csv_files[i_file])
+    v_id[i_file] <- i
+    v_nobs[i_file] <-  sum((!is.na(t$sulfate) & !is.na(t$nitrate)))
+    i_file <- i_file + 1
+  }
+  t <- data.frame(v_id, v_nobs)
+  names(t) <- c("id", "nobs")
+  t
+}
