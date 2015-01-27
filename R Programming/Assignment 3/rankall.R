12b29e11af7d49f8f5648feac43014cd403fdaed
Jaska <jaska@Jaakkos-Mac-mini.local>
2015-01-27 22:42:45 +0200

    Assignment 3, complete rewrite
    Experimented with data.frame, a lot faster

--- R Programming/Assignment 3/rankall.R
+++ R Programming/Assignment 3/rankall.R
@@ -1,71 +1,39 @@
+library(data.table)
+
 OUTCOME_COLUMNS <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
 
 get_colums_to_read <- function(outcome_col_num) {
-  
   read_cols <- c(rep("NULL", 46)) # don't read any columns by defauly
   read_cols[2] <- "character" # hospital name
   read_cols[7] <- "character" # state id
-  read_cols[OUTCOME_COLUMNS[outcome_col_num]] <- "character" # selected outcome column as string
+  read_cols[OUTCOME_COLUMNS[outcome_col_num]] <- "numeric" # double for data.table
   read_cols
-  
 }
 
 rankall <- function(outcome, num = "best") {
-  
-  org_num <- num
-  # get outcome column and do error handling
   col_num <- which(names(OUTCOME_COLUMNS) == outcome)
   if (identical(col_num, integer(0))) { stop("invalid outcome") }
   
+  row_position <- ifelse(is.numeric(num), num, 1) 
+
+  # use scoping rules to reduce arguments
+  get_hospital_by_rank <- function(rate_data) {
+    hotel_rate <- rate_data[row_position, rate]
+    x <- rate_data[rate == hotel_rate] # all hotels having this rate
+    setkey(x, hospital) # sort
+    x[1,hospital] # return first hospital
+  }
+  
   read_cols <- get_colums_to_read(col_num)
 
-  d <- read.csv("outcome-of-care-measures.csv", colClasses = read_cols)
-  
-  if (num == "best") { num <- 1 }
-  
-  d[,3] <- as.numeric(d[, 3])
-  l <- split(d, d$State) # 
+  data <- na.omit(fread("outcome-of-care-measures.csv", colClasses = read_cols, na.strings = "Not Available"))
+  setnames(data, c("hospital", "state", "rate"))
 
-  v1 <- character(0)
-  v2 <- character(0)
+  data$rate <- as.numeric(data$rate) # there has to be a way to read numeric data directly
+  ifelse(num == "worst", setorder(data, state, -rate), setorder(data, state, rate) ) # ugly
   
-  num_states = length(l)
-  for (i in 1:num_states)
-  {
-    data <- l[[i]]
-    state <- data[1,2]
-    print(state)
-    v2[i] <- state
-
-   #if (state == "NV") print(data)
-    data <- data[complete.cases(data),] # filter out NAs
-   
-   if (org_num == "worst") { num <- nrow(data) }
-   if (num > nrow(data)) { v1[i] <- NA }
-
-   #print(paste("num", num))
-   #print(paste("nrow", nrow(data)))
-   
-    data <- data[order(data[,3]),] # sort
-    #if (state == "NV") print(data)
-    
-    rate <- data[num,3]
-    
-    if (is.na(rate)) {
-      v1[i] <- NA
-    }
-    else
-    {
-      data <- subset(data, data[3] == rate)
-      data <- data[order(data[,1]),] # sort
-      
-      #print(data)
-      v1[i] <- data[1,1]
-    } 
-  }
-  rv <- data.frame(v1, v2)
-  colnames(rv) <- c("hospital", "state")
-  rownames(rv) <-  rv$state
-  rv
-  
+  x <- split(data, data$state)
+  x <- lapply(x, function(x) { get_hospital_by_rank(x) } )
+  data.frame(unlist(x))
 }
+
