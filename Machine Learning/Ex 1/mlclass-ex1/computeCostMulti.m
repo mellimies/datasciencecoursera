3a691e412e3c351733b14033e405d6f624ae47bb
Jaska <jaska@Jaakkos-Mac-mini.local>
2015-02-09 19:16:15 +0200

    ComputeCostMulti works
    Some other cleanup

--- Machine Learning/Ex 1/mlclass-ex1/computeCostMulti.m
+++ Machine Learning/Ex 1/mlclass-ex1/computeCostMulti.m
@@ -1,6 +1,6 @@
-function J = computeCost(X, y, theta)
-%COMPUTECOST Compute cost for linear regression
-%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
 % Initialize some useful values
@@ -14,7 +14,10 @@ J = 0;
 %               You should set J to the cost.
 
 h = sum(theta' .* X, 2);
+
 J = 1 / 2 / m * sum((h - y) .^2);
+
+
 
 % =========================================================================
 
--- Machine Learning/Ex 1/mlclass-ex1/featureNormalize.m
+++ Machine Learning/Ex 1/mlclass-ex1/featureNormalize.m
@@ -27,10 +27,9 @@ sigma = zeros(1, size(X, 2));
 %       
 
 for i = 1:columns(X)
-  i
-  mu(i) = mean(X(:,i))
-  sigma(i) = std(X(:,i))
-  X_norm(:,i) = (X(:,i) - mu(i)) / sigma(i)
+  mu(i) = mean(X(:,i));
+  sigma(i) = std(X(:,i));
+  X_norm(:,i) = (X(:,i) - mu(i)) / sigma(i);
 end
 
 
